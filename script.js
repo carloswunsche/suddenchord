906798cbb3c8ab8b4bf41aa3a4cc416da87587f2
chadsito <wunschecarlos@gmail.com>
2021-10-25 21:51:49 -0400

    Karl made some changes

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const chordArray = ['A','B','C','D','E','F','G'];
+let minor = false;
+
+const changeCurChord = function(newChord = 'C') {
+    //Switch between minor and major
+    minor = !minor;
+    if (minor) newChord = newChord + 'm';
+    //Display new chord
+    document.querySelector('.current').textContent = newChord;
+};
+
+
+const timer = setInterval(function() {
+    changeCurChord(chordArray[Math.trunc(Math.random() * chordArray.length)])
+}, 2000);
+
+
+// let stop = false;
+// if (stop) clearInterval(timer);

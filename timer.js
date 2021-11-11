1f083beab8baabe951b53c26ca862809fcf79055
Karl Wunsche <wunschecarlos@gmail.com>
2021-11-11 11:02:50 -0500

    Metronome 1.1

--- timer.js
+++ timer.js
@@ -6,6 +6,8 @@ function Timer(callback, timeInterval = 120, beatsPerMeasure) {
     this.click1 = new Howl({src: ['click01.ogg']});
     this.click2 = new Howl({src: ['click02.ogg']});
     this.timeInterval = 60000 / timeInterval;
+    this.beatsPerMeasure = beatsPerMeasure;
+    this.counter = 1;
 
     // Start method
     this.start = () => {
@@ -15,8 +17,7 @@ function Timer(callback, timeInterval = 120, beatsPerMeasure) {
         // Start the timeout and save the ID in a property, so we can cancel it later
         this.timeout = null;
         // Execute the callback function
-        callback();
-        this.click1.play()
+        this.callbackAndClick();
         // Re watch the tutorial to understand this xD
         this.timeout = setTimeout(this.round, this.timeInterval);
     };
@@ -36,10 +37,23 @@ function Timer(callback, timeInterval = 120, beatsPerMeasure) {
         //         errorCallback();
         //     };
         // };
-        callback();
-        this.click1.play()
+        this.callbackAndClick();
+        // Recalculate timeout
         this.expected += this.timeInterval;
         this.timeout = setTimeout(this.round, this.timeInterval - drift)
+    };
+
+    this.callbackAndClick = () => {
+        // Callback if beat 1
+        if (this.counter === 1) {
+            callback();
+            this.click1.play()
+        };
+        // If not, just play click2
+        if (this.counter !== 1) this.click2.play()
+        //Advance counter and reset if === beatsPerMeasure
+        this.counter++
+        if (this.counter === this.beatsPerMeasure + 1) this.counter = 1;
     };
 };
 
